3de738b5ff22a68890d98e33b26a6d2fd423cfc0
Kapil <kapilbehwal9555@gmail.com>
2024-05-25 19:45:17 +0530

    new home

--- app/newhome/page.js
+++ app/newhome/page.js
@@ -3,14 +3,14 @@ import { Box, Container, Grid, IconButton, Typography } from '@mui/material'
 import React from 'react'
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import Image from 'next/image';
-import foodstall from "../assests/foodStall.png"
-import googleplay from "../assests/playstore.png"
-import phone from "../assests/phone.jpg"
-import Footer from './Footer';
-import food1 from "../assests/food1.jpg"
-import food2 from "../assests/food2.jpeg"
-import food3 from "../assests/food3.jpeg"
-import food4 from "../assests/food3.jpg"
+import foodstall from "../../assests/foodStall.png"
+import googleplay from "../../assests/playstore.png"
+import phone from "../../assests/phone.jpg"
+import Footer from '../../components/Footer';
+import food1 from "../../assests/food1.jpg"
+import food2 from "../../assests/food2.jpeg"
+import food3 from "../../assests/food3.jpeg"
+import food4 from "../../assests/food3.jpg"
 // import food1 from "../assests/food1.jpg"
 
 import QrCode2Icon from '@mui/icons-material/QrCode2';
@@ -25,17 +25,17 @@ const HomePage = () => {
   return (
     <>
     <Container maxWidth={'xl'} disableGutters>
-        <Grid container justifyContent={'center'} sx={{p:"5px 10px",bgcolor:"black"}}>
-            <Grid item lg={6} md={6} sm={10} xs={12} sx={{mb:"15px",borderRadius:"10px",border:"5px solid #8bc34a",p:"0px 15px"}}>
+        <Grid container justifyContent={'center'} sx={{p:"0px 10px",bgcolor:"black"}}>
+            <Grid item lg={6} md={6} sm={10} xs={12} sx={{mb:"15px",bgcolor:"#8bc34a",p:"0px 15px 15px 0px"}}>
                 <Grid container sx={{mt:"40px"}}>
                     <Grid item xs={12} sx={{}}>
                         <Box sx={{display:"flex",justifyContent:"space-between",width:"100%"}}>
                             <Typography variant='h3' sx={{fontWeight:600,fontFamily:"sans-serif"}}>
-                                <span style={{color:"white"}}>Badi</span><span style={{color:"#8bc34a"}}>Dukkan</span>
+                                <span style={{color:"black"}}>Badi</span><span style={{color:"white"}}>Dukkan</span>
                             </Typography>
                             <Box>
-                            <IconButton sx={{bgcolor:"#8bc34a","&:hover":{bgcolor:"#8bc34a"},height:"fit-content"}}>
-                                <PersonOutlineIcon sx={{color:"black"}}/>
+                            <IconButton sx={{bgcolor:"black","&:hover":{bgcolor:"black"},height:"fit-content"}}>
+                                <PersonOutlineIcon sx={{color:"#8bc34a"}}/>
                             </IconButton>
                             </Box>
                         </Box>
@@ -44,37 +44,37 @@ const HomePage = () => {
                         <Grid container>
                             <Grid item xs={6}>
                                 <Box sx={{pr:"15px"}}>
-                                    <Typography variant='h4' sx={{color:"#bdbdbd"}}>
-                                        Now, trusted stores around you are on <span style={{fontWeight:600,color:"#8bc34a"}}>BadiDukkan.</span>
+                                    <Typography variant='h4' sx={{color:"black"}}>
+                                        Now, trusted stores around you are on <span style={{fontWeight:600}}>BadiDukkan.</span>
                                     </Typography>
-                                    <Typography variant='h6' sx={{color:"#bdbdbd"}}>
+                                    <Typography variant='h6' sx={{color:"#424242"}}>
                                         Your neighbourhood stores <br/> are now at your fingertips
                                     </Typography>
                                 </Box>
                             </Grid>
                             <Grid item xs={6}>
                                 <Box sx={{position:"relative",height:"100%",display:"flex",justifyContent:"center"}}>
-                                    <Image src={foodstall} alt='foodstall'  style={{position:"absolute",height:"170px",borderRadius:"10px",width:"auto",border:"2px solid #8bc34a"}}/>
+                                    <Image src={foodstall} alt='foodstall'  style={{position:"absolute",height:"170px",width:"auto"}}/>
                                 </Box>
                             </Grid>
                             <Grid item xs={12}>
                                 <Box sx={{position:"relative",display:"flex",justifyContent:"center",alignItems:"center",height:"150px"}}>
-                                    <Image src={googleplay} alt='foodstall' style={{height:"40px",width:"auto",position:"absolute",borderRadius:"10px",border:"2px solid #8bc34a"}}/>
+                                    <Image src={googleplay} alt='foodstall' style={{height:"40px",width:"auto",position:"absolute"}}/>
                                 </Box>
                             </Grid>
                             <Grid item xs={12}>
                                 <Grid container>
                                     <Grid item xs={7} sx={{height:"350px",display:"flex",alignItems:"center"}}>
                                         <Box sx={{boxSizing:"border-box"}}>
-                                            <Typography variant='h5' sx={{color:"#bdbdbd"}}>
+                                            <Typography variant='h5' sx={{color:"black"}}>
                                                 One place <span style={{fontWeight:600}}>biryani, pizza</span>and<span style={{fontWeight:600}}>much more.</span>
                                             </Typography>
-                                            <Typography variant='body2' sx={{color:"#bdbdbd"}}>Find everything you need in one! Explore and shop for the best quality snacks and meals</Typography>
+                                            <Typography variant='body2' sx={{color:"#424242"}}>Find everything you need in one! Explore and shop for the best quality snacks and meals</Typography>
                                         </Box>
                                     </Grid>
                                     <Grid item xs={5} sx={{display:"flex",alignItems:"center",justifyContent:"center"}}>
                                         <Box sx={{position:"relative",height:{lg:"350px",md:"350px",sm:"300px",xs:"230px"},width:"fit-content"}}>
-                                            <Image src={food1} alt='foodstall' style={{height:"100%",borderRadius:"15px",width:"auto",boxSizing:"border-box",borderRadius:"10px",border:"3px solid #8bc34a"}}/>
+                                            <Image src={food1} alt='foodstall' style={{height:"100%",borderRadius:"15px",width:"auto",boxSizing:"border-box"}}/>
                                         </Box>
                                     </Grid>
                                 </Grid>
@@ -84,16 +84,16 @@ const HomePage = () => {
                                 <Grid container>
                                 <Grid item xs={5} sx={{display:"flex",alignItems:"center",justifyContent:"center"}}>
                                         <Box sx={{position:"relative",height:{lg:"350px",md:"350px",sm:"300px",xs:"230px"},width:"fit-content"}}>
-                                            <Image src={food2} alt='foodstall' style={{height:"100%",borderRadius:"15px",width:"auto",boxSizing:"border-box",borderRadius:"10px",border:"3px solid #8bc34a"}}/>
+                                            <Image src={food2} alt='foodstall' style={{height:"100%",borderRadius:"15px",width:"auto",boxSizing:"border-box"}}/>
                                         </Box>
                                     </Grid>
                                 
                                     <Grid item xs={7} sx={{height:"350px",display:"flex",alignItems:"center"}}>
                                         <Box sx={{boxSizing:"border-box",p:"10px"}}>
-                                            <Typography variant='h5' sx={{color:"#bdbdbd"}}>
+                                            <Typography variant='h5' sx={{color:"black"}}>
                                                 Explore the <span style={{fontWeight:600}}>widest range of products</span>
                                             </Typography>
-                                            <Typography variant='body2' sx={{color:"#bdbdbd"}}>Checkout and choose from a variety of foods available from restaurants across the city</Typography>
+                                            <Typography variant='body2' sx={{color:"#424242"}}>Checkout and choose from a variety of foods available from restaurants across the city</Typography>
                                         </Box>
                                     </Grid>
                                     </Grid>
@@ -104,15 +104,15 @@ const HomePage = () => {
                                 <Grid container>
                                     <Grid item xs={7} sx={{height:"350px",display:"flex",alignItems:"center"}}>
                                         <Box sx={{boxSizing:"border-box",p:"10px"}}>
-                                            <Typography variant='h5' sx={{color:"#bdbdbd"}}>
+                                            <Typography variant='h5' sx={{color:"black"}}>
                                                Shop from <span style={{fontWeight:600}}>local trusted restaurants</span>around you
                                             </Typography>
-                                            <Typography variant='body2' sx={{color:"#bdbdbd"}}>from pizza to butter panner, order from local restaurants you trust.</Typography>
+                                            <Typography variant='body2' sx={{color:"#424242"}}>from pizza to butter panner, order from local restaurants you trust.</Typography>
                                         </Box>
                                     </Grid>
                                     <Grid item xs={5} sx={{display:"flex",alignItems:"center",justifyContent:"center"}}>
                                         <Box sx={{position:"relative",height:{lg:"350px",md:"350px",sm:"300px",xs:"230px"},width:"fit-content"}}>
-                                            <Image src={food3} alt='foodstall' style={{height:"100%",borderRadius:"15px",width:"auto",boxSizing:"border-box",borderRadius:"10px",border:"3px solid #8bc34a"}}/>
+                                            <Image src={food3} alt='foodstall' style={{height:"100%",borderRadius:"15px",width:"auto",boxSizing:"border-box"}}/>
                                         </Box>
                                     </Grid>
                                 </Grid>
@@ -122,16 +122,16 @@ const HomePage = () => {
                                 <Grid container>
                                 <Grid item xs={5} sx={{display:"flex",alignItems:"center",justifyContent:"center"}}>
                                         <Box sx={{position:"relative",height:{lg:"350px",md:"350px",sm:"300px",xs:"230px"},width:"fit-content"}}>
-                                            <Image src={food4} alt='foodstall' style={{height:"100%",borderRadius:"15px",width:"auto",boxSizing:"border-box",borderRadius:"10px",border:"3px solid #8bc34a"}}/>
+                                            <Image src={food4} alt='foodstall' style={{height:"100%",borderRadius:"15px",width:"auto",boxSizing:"border-box"}}/>
                                         </Box>
                                     </Grid>
                                 
                                     <Grid item xs={7} sx={{height:"350px",display:"flex",alignItems:"center"}}>
                                         <Box sx={{boxSizing:"border-box",p:"10px"}}>
-                                            <Typography variant='h5' sx={{color:"#bdbdbd"}}>
+                                            <Typography variant='h5' sx={{color:"black"}}>
                                                 You can now <span style={{fontWeight:600}}>preorder</span>the food.
                                             </Typography>
-                                            <Typography variant='body2' sx={{color:"#bdbdbd"}}>Pre-order your food as per your convenience.</Typography>
+                                            <Typography variant='body2' sx={{color:"#424242"}}>Pre-order your food as per your convenience.</Typography>
                                         </Box>
                                     </Grid>
                                     </Grid>
